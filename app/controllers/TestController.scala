800670160ce202fea2c929dee3b4049138c6dd2e
Nicole Avison <nicole.avison@digital.hmrc.gov.uk>
2018-10-18 11:37:50 +0100

    testing database connection...

--- app/controllers/TestController.scala
+++ app/controllers/TestController.scala
@@ -5,26 +5,27 @@ import javax.inject.Inject
 
 import org.mongodb.scala.bson.collection.Document
 import org.mongodb.scala.{Completed, MongoClient, MongoCollection, MongoDatabase, Observable, Observer}
+import play.api.Configuration
 import play.api.mvc.{AbstractController, AnyContent, ControllerComponents, Request}
 
 class TestController @Inject() (
                                components: ControllerComponents,
+                               config: Configuration
                              ) extends AbstractController(components) with App {
 
-  val databaseUri = System.getenv("mongodb.uri")
 
-  val mongoClient: MongoClient = MongoClient(databaseUri)
 
-  val database: MongoDatabase = mongoClient.getDatabase("mydb")
-
-  val collection: MongoCollection[Document] = database.getCollection("test")
-
-  val doc: Document = Document("_id" -> LocalDateTime.now.toString, "name" -> "MongoDB", "type" -> "database",
-    "count" -> 1, "info" -> Document("x" -> 203, "y" -> 102))
 
   def test() = Action { implicit request: Request[AnyContent] =>
 
-    println("database uri: " + databaseUri)
+    val databaseUri = config.get[String]("mongodb.uri")
+    val mongoClient: MongoClient = MongoClient(databaseUri)
+    val database: MongoDatabase = mongoClient.getDatabase("mydb")
+    val collection: MongoCollection[Document] = database.getCollection("test")
+    val doc: Document = Document("_id" -> LocalDateTime.now.toString, "name" -> "MongoDB", "type" -> "database",
+      "count" -> 1, "info" -> Document("x" -> 203, "y" -> 102))
+
+    println("database: " + databaseUri)
 
     val observable: Observable[Completed] = collection.insertOne(doc)
 
